3e141cec4db99c6fe3716c29caa16f28632ecc33
Daniele Zurico <daniele.zurico@blackswan.com>
2017-01-18 15:37:03 +0000

    update to 2.4.3

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,50 @@
 const ngToolsWebpack = require('@ngtools/webpack');
+
 //var CopyWebpackPlugin = require('copy-webpack-plugin');
 var webpack = require('webpack');
 
 module.exports = {
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  entry: './app/main.aot.ts',
-  output: {
-    path: './dist',
-    publicPath: 'dist/',
-    filename: 'app.main.js'
-  },
-  plugins: [
-    new ngToolsWebpack.AotPlugin({
-      tsConfigPath: './tsconfig.json'
-    }),
-		//new CopyWebpackPlugin([
-		//	{from: './index.html'}
-		//]),
-		new webpack.LoaderOptionsPlugin({
-			minimize: true,
-			debug: false
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false
-			},
-			output: {
-				comments: false
-			},
-			sourceMap: true
-		})
-  ],
-  module: {
-    loaders: [
-      { test: /\.scss$/, loaders: ['raw-loader', 'sass-loader'] },
-      { test: /\.css$/, loader: 'raw-loader' },
-      { test: /\.html$/, loader: 'raw-loader' },
-      { test: /\.ts$/, loader: '@ngtools/webpack' }
-    ]
-  },
-  devServer: {
-    historyApiFallback: true
-  }
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    entry: './app/main.aot.ts',
+    output: {
+        path: './dist',
+        publicPath: 'dist/',
+        filename: 'app.main.js'
+    },
+    plugins: [
+        new ngToolsWebpack.AotPlugin({
+            tsConfigPath: './tsconfig.json',
+            entryModule: __dirname + '/app/app.module#AppModule'
+        }),
+        //new CopyWebpackPlugin([
+        //	{from: './index.html'}
+        //]),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            output: {
+                comments: false
+            },
+            sourceMap: true
+        })
+    ],
+    module: {
+        loaders: [
+            {test: /\.scss$/, loaders: ['raw-loader', 'sass-loader']},
+            {test: /\.css$/, loader: 'raw-loader'},
+            {test: /\.html$/, loader: 'raw-loader'},
+            {test: /\.ts$/, loader: '@ngtools/webpack'}
+        ]
+    },
+    devServer: {
+        historyApiFallback: true
+    }
 };
+
